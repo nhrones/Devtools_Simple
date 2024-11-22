da3a6658e391cb5fcff2b826a422f6f18ea0be44
NDH <ndhrones@gmail.com>
2024-11-22 17:58:58 -0500

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 export { join } from "jsr:@std/path";
 export { serveFile } from "jsr:@std/http";
-export * from "https://raw.githubusercontent.com/nhrones/Devtools_Browser/refs/heads/main/browser.ts"  //"../Browser/browser.ts"
-export * from "https://raw.githubusercontent.com/nhrones/Devtools_Config/refs/heads/main/mod.ts" //"../Config/mod.ts"
+export * from "https://raw.githubusercontent.com/nhrones/Devtools_Browser/refs/heads/main/browser.ts";
+export * from "https://raw.githubusercontent.com/nhrones/Devtools_Config/refs/heads/main/mod.ts";
