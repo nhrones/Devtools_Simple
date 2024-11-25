f301d822809e9eebcc60be31d1e99cab796bccd7
NDH <ndhrones@gmail.com>
2024-11-25 15:43:00 -0500

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { openWebsite } from "jsr:@ndh/browser";
+export { openWebsite } from "jsr:@ndh/browser@1.0.2";
 export { join } from "jsr:@std/path@1.0.8";
 export { serveFile } from "jsr:@std/http@1.0.11";
