ec3896283232164871b15f9d1ae4463050161ad2
NDH <nick@ndhrones.com>
2024-12-31 15:11:49 -0500

    update

--- config.ts
+++ config.ts
@@ -39,7 +39,7 @@ const requiredCfg = {
 } satisfies Config
 
 // gets an existing config, or builds one
-const cfg = getConfig('simple', "1.0.6", Deno.args, requiredCfg)
+const cfg = getConfig('RUN', "1.0.7", Deno.args, requiredCfg)
 
 export const DEV = cfg.DEV || false
 export const folder = cfg.Serve || ""
