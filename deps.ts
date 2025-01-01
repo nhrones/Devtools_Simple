cae36ff7621d9f70aadbe2f907b45a1458f41cd8
NDH <nick@ndhrones.com>
2025-01-01 13:43:33 -0500

    update

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export type { Config } from "jsr:@ndh/config@1.0.9";
-export { getConfig }from "jsr:@ndh/config@1.0.9";
+export type { Config } from "jsr:@ndh/config@1.0.10";
+export { getConfig }from "jsr:@ndh/config@1.0.10";
 export { join } from "jsr:@std/path@1.0.8";
 export { serveFile } from "jsr:@std/http@1.0.11";
