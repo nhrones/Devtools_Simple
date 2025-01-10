2293d8e4896c873d78b7c2ad06ba4b4d2aba03e5
NDH <nick@ndhrones.com>
2025-01-10 12:34:08 -0500

    update

--- config.ts
+++ config.ts
@@ -2,34 +2,6 @@ import { Config, getConfig }from "./deps.ts";
 import JSR from "./jsr.json" with { type: "json" };
 export const VER = JSR.version
 
-if (Deno.args[0]) {
-   // if args0 = -h or ?, show help then exit
-   if (Deno.args[0] === '-h' || Deno.args[0] === '?') {
-      console.log(`
-   Simple Server Help --
-   
-   Usage:
-
-   commandline args:
-
-   arg[0]:
-   -h or ? = this help
-   if number = port number to use
-   if string = folder to be served
-
-   arg[1]:
-   if arg[0] = folder then arg[1] (number) = port number to use
-   if arg[0] = port number then arg[1] = (string) folder to be served
-
-   This command uses/mutates the 'simple' entry in ./.vscode/dev.json
-   dev.json:
-   BuildTarget: "",
-   Port: 80
-   `);
-      Deno.exit(0)
-   }
-}
-
 /** required configuration */
 const requiredCfg = {
    "Serve": "",
--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export type { Config } from "jsr:@ndh/config@1.1.2";
-export { getConfig }from "jsr:@ndh/config@1.1.2";
+export type { Config } from "jsr:@ndh/config@1.1.4";
+export { getConfig }from "jsr:@ndh/config@1.1.4";
 export { join } from "jsr:@std/path@1.0.8";
 export { serveFile } from "jsr:@std/http@1.0.11";
